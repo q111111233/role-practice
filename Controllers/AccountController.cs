243ee12d4aef1d78ec8c856c59867d0df5655fac
Adam Luchini and Bo Zhao <adam+bo@>
2016-09-14 15:50:20 -0700

    roles edition

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using BasicAuthentication.Models;
 using BasicAuthentication.ViewModels;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Linq;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace BasicAuthentication.Controllers
 {
@@ -13,10 +16,12 @@ namespace BasicAuthentication.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
+        public AccountController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db)
         {
+            _roleManager = roleManager;
             _userManager = userManager;
             _signInManager = signInManager;
             _db = db;
@@ -66,6 +71,61 @@ namespace BasicAuthentication.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(RoleViewModel model)
+        {
+
+                var role = new IdentityRole { Name = model.RoleName };
+                IdentityResult result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Role()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Role");
+        }
+
+        public ActionResult Edit(string RoleName)
+        {
+            var thisRole = _db.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            return View(thisRole);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IdentityRole role)
+        {
+            try
+            {
+                _db.Entry(role).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Role");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -8,6 +8,6 @@ namespace BasicAuthentication.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+        
     }
 }
